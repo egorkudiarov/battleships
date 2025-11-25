531193477623ff9c61acb841d351d71b16957094
egorkudiarov <egerkud@gmail.com>
2025-11-25 09:28:19 +0100

    renamed test_funcs.h to testfuncs.h, moved printships from main.c to testfuncs.h, created struct.h to store structures, created loggers.h, added templates to game state logger and turn logger

---
+++ loggers.h
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "structs.h"
+
+#ifndef LOGGERS_H_INCLUDED
+#define LOGGERS_H_INCLUDED
+
+#define DIMX 8
+#define DIMY 8
+#define MAXLEN 5
+#define MINLEN 2
+#define SHIPCOUNT 5
+
+void board_copy(FILE *main, FILE *temp){
+    char line[DIMX];
+    for(int i = 0; i < (DIMY*2); i++){
+        fscanf(main, "%s", &line);
+        fprintf(temp, "%s\n", line);
+    }
+}
+
+void board_write(FILE *main, FILE *temp, int player_field[DIMX][DIMY], int sub_player_field[DIMX][DIMY]){
+    char line[DIMX];
+    for(int i = 0; i < DIMY; i++){
+        for(int j = 0; i < DIMY; i++){
+
+            fprintf(temp, "%d",player_field[i][j]);
+        }
+        fscanf(main, "%s", &line);
+        fprintf(temp, "\n");
+    }
+    for(int i = 0; i < DIMY; i++){
+        for(int j = 0; i < DIMY; i++){
+            fprintf(temp, "%d",sub_player_field[i][j]);
+        }
+        fscanf(main, "%s", &line);
+        fprintf(temp, "\n");
+    }
+}
+
+void ships_write(FILE *file, Ship *ships){
+    for(int i = 0; i < SHIPCOUNT; i++){
+        printf("%d %d %d %d %d\n", ships[i].x, ships[i].y, ships[i].lenght, ships[i].derection, ships[i].is_destroed);
+    }
+}
+
+int game_state_log(char *filename, int *player, int player_field[DIMX][DIMY], int sub_player_field[DIMX][DIMY], Ship *players_ships, Ship *computer_ships){
+    FILE *temp, *main;
+    temp = fopen("temp.txt", "w");
+    main = fopen(filename, "r");
+    switch(*player){
+        case (0):{
+            board_copy(main, temp);
+            board_write(main, temp, player_field, sub_player_field);
+            break;
+        }
+        case (1):{
+            board_write(main, temp, player_field, sub_player_field);
+            board_copy(main, temp);
+            break;
+        }
+        default: return 1;
+    }
+    fclose(temp);
+    fclose(main);
+    temp = fopen("temp.txt", "r");
+    main = fopen(filename, "w");
+    board_copy(temp, main);  
+    board_copy(temp, main);  
+    ships_write(main, players_ships);
+    ships_write(main, computer_ships);
+    fclose(temp);
+    fclose(main);
+    return 0;
+}
+
+void game_turn_log(char *filename, int *player, int *x, int *y, int *result){
+    FILE *file;    
+    file = fopen(filename, "a");
+    fprintf(file, "%d|%d %d|%d", player, x, y, result);
+    fclose(file);
+}
+#endif
---
+++ structs.h
@@ -0,0 +1,8 @@
+#ifndef STRUCTS_H_INCLUDED
+#define STRUCTS_H_INCLUDED
+
+typedef struct {
+	short x, y, lenght, derection, is_destroed;
+}Ship;
+
+#endif
